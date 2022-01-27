6c4c2641a5fb32eda61a15eb36569bee64c73030
Nick Sullivan <nicholassull@gmail.com>
2022-01-27 15:42:10 -0800

    add buttons to landing page and make 'search' button show the search section

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import 'animate.css';
 import GiphyService from './js/giphy-search';
 
 $(document).ready(function() {
+  $('#landing-search-button').click(function() {
+    $('#landing-wrapper').toggle();
+    $('#search-wrapper').fadeIn();
+  });
   $("#button").click(function() {
     event.preventDefault();
     let searchTerm = $('#search-input').val();
+    $('#img-wrapper').empty();
     GiphyService.gifSearch(searchTerm)
       .then(function(response) {
         for (let i = 0; i < 10; i++) {
