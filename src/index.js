cf890b15aacc804582389327716fe7f8aee77894
Nick Sullivan <nicholassull@gmail.com>
2022-01-27 16:39:26 -0800

    dry up index.js

--- src/index.js
+++ src/index.js
@@ -5,21 +5,26 @@ import './css/styles.css';
 import 'animate.css';
 import GiphyService from './js/giphy-search';
 
+function displaySearchResults(searchTerm) {
+  GiphyService.gifSearch(searchTerm)
+      .then(function(response) {
+        for (let i = 0; i < 15; i++) {
+          let url = response.data[i].images.fixed_height_small.url;
+          $("#img-wrapper").append(`<img id="image${i}" class="gif-thumbnail" src="${url}">`);
+        }
+      });
+}
+
 $(document).ready(function() {
   $('#landing-search-button').click(function() {
     $('#landing-wrapper').toggle();
     $('#search-wrapper').fadeIn();
   });
-  $("#button").click(function() {
+  
+  $("#search-button").click(function() {
     event.preventDefault();
     let searchTerm = $('#search-input').val();
     $('#img-wrapper').empty();
-    GiphyService.gifSearch(searchTerm)
-      .then(function(response) {
-        for (let i = 0; i < 10; i++) {
-          let url = response.data[i].images.fixed_height_small.url;
-          $("#img-wrapper").append(`<img class="gif-thumbnail" src="${url}">`);
-        }
-      });
+    displaySearchResults(searchTerm);
   });
 });
